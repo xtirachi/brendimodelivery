f97620a3725ad0e5ee3232aff5d7621608f91497
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-10-11 12:08:03 +0400

    Update ordercreation.js

--- ordercreation.js
+++ ordercreation.js
@@ -23,7 +23,7 @@ document.getElementById('orderForm').addEventListener('submit', function(e) {
     }
 
     // Send the data to Google Apps Script to create the order
-    fetch('https://script.google.com/macros/s/AKfycbxqcUH_fxIcwiGF3-Hbb24QhpoHjq86uXSVsuqASZEJwMC0dAdYl572N9eOQ5dmJBvo/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbwWWrA-0XYKjYZ6TpAgdRHBrWP8hEh0dNQOE6umAw_maoFzaR3F7TpmXtDwQuxXztk0/exec', {
         method: 'POST',
         body: new URLSearchParams({
             action: 'createOrder',
