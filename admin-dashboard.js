26851417b30c5b56b6f71240158cdb3eb0ca554e
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-12-24 15:15:55 +0400

    Update admin-dashboard.js

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -29,8 +29,8 @@ function loadOrdersByDate(date) {
           const orderDetails = order[4];  // Assuming Column E contains Order Details (Məhsullar, Miqdar)
 
           // Only count the order in the total amount if it is not canceled
-          if (status !== 'Canceled' || status === 'Deleted') {
-            totalAmount += orderAmount;
+          if (status === 'Deleted') {
+            totalAmount += (orderAmount-6);
           }
 
 if (status === 'Delivered' || status === 'Canceled') {
